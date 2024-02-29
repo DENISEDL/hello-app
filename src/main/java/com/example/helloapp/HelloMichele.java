9ebe5f7f93715fd0cf9cd4f79268c22b95c151f4
DENISE.DL <denise.dl@hotmail.it>
2024-02-29 17:12:46 +0100

    Ciao-MICHELE

--- src/main/java/com/example/helloapp/HelloMichele.java
+++ src/main/java/com/example/helloapp/HelloMichele.java
@@ -1,28 +1,4 @@
 package com.example.helloapp;
 
-<<<<<<< HEAD
 public class HelloMichele {
-=======
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/v1")
-public class HelloMichele {
-
-    //METODO SENZA PARAMETRI
-    @GetMapping("/ciaoMichele")
-    public String saluta(){
-        return "Ciao Michele!";
-    }
-    //METODO CON PASSAGGIO DI UN PARAMETRO
-    @GetMapping("/ciaoTutor")
-    public String saluta(@RequestParam String nome){
-        return  "<h1>Ciao , " + nome + " !</h1>";
-    }
-    //METODO CON IL PASSAGGIO DI UN OGGETTO
-    @GetMapping("/ciaoUser")
-    public User saluta(@RequestBody User userCreato){
-        return userCreato;
-    }
->>>>>>> b021c9f (Ciao-MICHELE)
 }
