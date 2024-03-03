de4b2eea76de6813ec012a33196b2f2f0ed42be1
DENISE.DL <denise.dl@hotmail.it>
2024-03-03 19:31:52 +0100

    2eser

--- src/main/java/com/example/helloapp/HelloMichele.java
+++ src/main/java/com/example/helloapp/HelloMichele.java
@@ -1,28 +1,4 @@
 package com.example.helloapp;
 
-import org.springframework.web.bind.annotation.*;
-@RestController
-@RequestMapping("/v1/ciao")
 public class HelloMichele {
-
-
-        //METODO SENZA PARAMETRI
-        @GetMapping("/ciaoMichele")
-        public String saluta() {
-            return "Ciao Michele!";
-        }
-
-        //METODO CON PASSAGGIO DI UN PARAMETRO
-        @GetMapping("/ciaoTutor")
-        public String saluta(@RequestParam String nome,@RequestParam String provincia) {
-            return "Ciao , " + nome + " com'è il tempo in " + provincia + "?";
-        }
-
-        //METODO CON IL PASSAGGIO DI UN OGGETTO
-        @GetMapping("/ciaoUser")
-        public User saluta(@RequestBody User userCreato) {
-            return userCreato;
-        }
-    }
-
-
+}
--- src/main/java/com/example/helloapp/User.java
+++ src/main/java/com/example/helloapp/User.java
@@ -3,10 +3,14 @@ package com.example.helloapp;
 public class User {
     private String nome;
     private String cognome;
+    private String provincia;
+    private String saluto;
 
-    public User(String nome, String cognome) {
+    public User(String nome, String cognome,String provincia,String saluto) {
         this.nome = nome;
         this.cognome = cognome;
+        this.provincia=provincia;
+        this.saluto= saluto;
     }
 
     public String getNome() {
@@ -23,5 +27,21 @@ public class User {
 
     public void setCognome(String cognome) {
         this.cognome = cognome;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getSaluto() {
+        return saluto;
+    }
+
+    public void setSaluto(String saluto) {
+        this.saluto = saluto;
     }
 }
